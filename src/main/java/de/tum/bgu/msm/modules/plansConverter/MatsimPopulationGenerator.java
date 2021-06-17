53a4ac6a9ad8889f60452739b1e247afdaeb4a84
Qinnnnn <cindy.qinzhang@gmail.com>
2021-06-17 19:51:40 +0200

    in MatsimPopulationGenerator, convert mito gender to matsim gender code

--- src/main/java/de/tum/bgu/msm/modules/plansConverter/MatsimPopulationGenerator.java
+++ src/main/java/de/tum/bgu/msm/modules/plansConverter/MatsimPopulationGenerator.java
@@ -53,8 +53,8 @@ public final class MatsimPopulationGenerator extends Module {
             try {
                 if (modeSet.contains(trip.getTripMode()) && !trip.getTripPurpose().equals(Purpose.RRT)) {
                     Person person = factory.createPerson(Id.createPersonId(trip.getId()));
-                    person.getAttributes().putAttribute("age",trip.getPerson().getAge());
-                    person.getAttributes().putAttribute("sex",trip.getPerson().getMitoGender());
+                    person.getAttributes().putAttribute("age",trip.getPerson().getAge()>100?100:trip.getPerson().getAge());
+                    person.getAttributes().putAttribute("sex",trip.getPerson().getMitoGender().equals(MitoGender.MALE)?"m":"f");
                     trip.setMatsimPerson(person);
 
                     Plan plan = factory.createPlan();
